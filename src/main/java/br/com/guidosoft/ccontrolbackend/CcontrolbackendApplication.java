7abd5ef82b5a5d3a2a81692db29f746a16ed27e5
Idair F. Guido <idair_guido@hotmail.com>
2023-08-22 17:58:02 -0300

    feat: implemented address repository and controller with endpoints getAll and insert

---
+++ src/main/java/br/com/guidosoft/ccontrolbackend/CcontrolbackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.guidosoft.ccontrolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcontrolbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcontrolbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guidosoft/ccontrolbackend/CcontrolbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guidosoft.ccontrolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcontrolbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
